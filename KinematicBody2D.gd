99f481a298136dba97f2d4c98991e0716d605a58
Prokop Havlík <maraxencz1097@gmail.com>
2023-03-08 20:45:27 +0100

    improved movement

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,33 +1,33 @@
 extends KinematicBody2D
 
-const UP = Vector2(0,-1)
-const GRAVITY = 20
-const MAXFALLSPEED = 200
-const MAXSPEED = 200
-const JUMPFORCE = 300
-const ACCEL = 10
+export var speed = 200
+export var jump_power_min = 200
+export var jump_power_max = 900
+export var gravity = 1200
+export var acc = 5
 
-var motion = Vector2()
+var motion = Vector2.ZERO
+var is_jumping = false
+var jump_power = jump_power_min
 
-func _ready():
-	pass
-	
 func _physics_process(delta):
-	
-	motion.y += GRAVITY
-	if motion.y > MAXFALLSPEED:
-		motion.y = MAXFALLSPEED
-	
-	motion.x = clamp (motion.x,-MAXSPEED,MAXSPEED)
-	
-	if Input.is_action_pressed("Right"):
-		motion.x += ACCEL
-	elif Input.is_action_pressed("Left"):
-		motion.x -= ACCEL
+	motion.y += gravity * delta
+	if Input.is_action_pressed("right"):
+		motion.x = min(motion.x + acc, speed)
+	elif Input.is_action_pressed("left"):
+		motion.x = max(motion.x - acc, -speed)
 	else:
-		motion.x = lerp(motion.x,0,0.2)
-		
-	if Input.is_action_just_pressed("jump"):
-		motion.y =-JUMPFORCE
-		
-	motion = move_and_slide(motion,UP)
+		motion.x = 0
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		is_jumping = true
+
+	if is_jumping and Input.is_action_pressed("jump") and jump_power < jump_power_max:
+		jump_power += delta * 500
+
+	if is_jumping and Input.is_action_just_released("jump"):
+		motion.y = -jump_power
+		is_jumping = false
+		jump_power = jump_power_min
+
+	motion = move_and_slide(motion, Vector2.UP)
