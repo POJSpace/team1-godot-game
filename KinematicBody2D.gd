f3ca68c85f998fbc6f6c57fa83bffad08847f4db
Majko <play.majko612@gmail.com>
2023-03-10 15:48:06 +0100

    Markus repair movment and jump

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
-export var speed = 200
+export var speed = 150
 export var jump_power_min = 200
 export var jump_power_max = 900
 export var gravity = 1200
-export var acc = 100
-export var deacc = 50
+export var acc = 300
+export var deacc = 800 #akcelerace zastavení
+export var jump_charge = 500
 
 var motion = Vector2.ZERO
 var is_jumping = false
@@ -13,31 +14,42 @@ var jump_power = jump_power_min
 
 func _physics_process(delta):
 	motion.y += gravity * delta
-	if Input.is_action_pressed("right"):
-		motion.x += acc * delta 
-		if motion.x > speed:
-			motion.x = speed
-	elif Input.is_action_pressed("left"):
-		motion.x -= acc * delta 
-		if motion.x < -speed:
-			motion.x = -speed
-	if motion.x > 0:
-		motion.x -= deacc * delta
-	elif motion.x < 0:
-		motion.x += deacc * delta
+	if is_on_floor():
+		if Input.is_action_pressed("right"):
+			motion.x += acc * delta 
+			if motion.x > speed:
+				motion.x = speed
+		elif Input.is_action_pressed("left"):
+			motion.x -= acc * delta 
+			if motion.x < -speed:
+				motion.x = -speed
+		else:
+			if motion.x < -5:
+				motion.x += deacc * delta
+			elif motion.x > 5:
+				motion.x -= deacc * delta
+			else:
+				motion.x = 0
+	
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		is_jumping = true
 
-	if is_jumping and Input.is_action_pressed("jump") and jump_power < jump_power_max:
-		jump_power += delta * 500
+	if is_jumping and Input.is_action_pressed("jump"):
+		if  jump_power < jump_power_max:
+			jump_power += delta * jump_charge
 		motion.x = 0
-	
+		
 	if is_jumping and Input.is_action_just_released("jump"):
 		if Input.is_action_pressed("right"):
 			motion.x += jump_power
+			
+			if motion.x > speed:
+				motion.x = speed
 		if Input.is_action_pressed("left"):
 			motion.x -= jump_power
+			if motion.x < -speed:
+				motion.x = -speed
 		motion.y = -jump_power
 		is_jumping = false
 		jump_power = jump_power_min
